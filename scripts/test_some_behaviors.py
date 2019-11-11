0e6e3745eb52b7fbb822a697bd399c80a46252eb
rramnauth2220 <rebecca.ramnauth@yale.edu>
2019-11-11 16:30:07 -0500

    updated sim behaviors

--- scripts/test_some_behaviors.py
+++ scripts/test_some_behaviors.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+'''
 import rospy
 from std_msgs.msg import String
 
@@ -18,3 +19,36 @@ if __name__ == '__main__':
         rospy.loginfo(msg)
         pub.publish(String(msg))
         rospy.sleep(2.0)
+'''
+
+import rospy
+from std_msgs.msg import String
+
+
+if __name__ == '__main__':
+    rospy.init_node("test_behaviors")
+    rospy.loginfo("Sending test commands")
+    pub = rospy.Publisher("/maki_macro", String, queue_size=10)
+    
+    while True:
+        n = raw_input("\n Input message:")
+        if (n == "shake"):
+            msgs = [
+                "reset eyelids" ,
+                #"spontaneousBlink start",
+                #"nod",
+                "shake"
+            ]
+        # FILL IN REMAINING COMMANDS
+        for msg in msgs:
+            rospy.loginfo(msg)
+            pub.publish(String(msg))
+            rospy.sleep(2.0)
+    '''msgs = [
+        "reset eyelids" ,
+        #"spontaneousBlink start",
+        #"nod",
+        "shake"
+    ]'''
+
+    
