eb45f598d5677be4330765865d6d099bc9a6d24e
rramnauth2220 <rebecca.ramnauth@yale.edu>
2019-11-12 11:10:42 -0500

    creating thinking behaviors

--- scripts/test_some_behaviors.py
+++ scripts/test_some_behaviors.py
@@ -24,31 +24,48 @@ if __name__ == '__main__':
 import rospy
 from std_msgs.msg import String
 
-
 if __name__ == '__main__':
     rospy.init_node("test_behaviors")
     rospy.loginfo("Sending test commands")
     pub = rospy.Publisher("/maki_macro", String, queue_size=10)
-    
+
+    default = [ # default behaviors
+        "reset eyelids",
+        "spontaneousBlink start"
+    ]
+
+    for msg in default: 
+        rospy.loginfo(msg)
+        pub.publish(String(msg))
+        rospy.sleep(1.0)
+
     while True:
         n = raw_input("\n Input message:")
-        if (n == "shake"):
+        if (n == "think"): # 'deep thought' behavior
             msgs = [
-                "reset eyelids" ,
-                #"spontaneousBlink start",
-                #"nod",
+                "lookAtAlissa" 
+            ]
+        elif (n == "wonder"): # 'wondering' behavior
+            msgs = [
+                "lookAwayFromAlissa"
+            ]
+        elif (n == "neutral"): # return to neutral position
+            msgs = [
+                "lookNeutral"
+            ]
+        elif (n == "shake"): # 'no' behavior
+            msgs = [
+                "reset eyelids",
                 "shake"
             ]
-        # FILL IN REMAINING COMMANDS
+        elif (n == "nod"): # 'yes' behavior
+            msgs = [
+                "reset eyelids",
+                "nod"
+            ]
+
         for msg in msgs:
             rospy.loginfo(msg)
             pub.publish(String(msg))
-            rospy.sleep(2.0)
-    '''msgs = [
-        "reset eyelids" ,
-        #"spontaneousBlink start",
-        #"nod",
-        "shake"
-    ]'''
-
+            rospy.sleep(1.0)
     
