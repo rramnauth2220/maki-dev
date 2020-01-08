f0a2eadf2c0c38dabcbc2a06053ad7b1e53d7b38
rramnauth2220 <rebecca.ramnauth@yale.edu>
2020-01-08 15:22:21 -0500

    updated logging

--- scripts/test_some_behaviors.py
+++ scripts/test_some_behaviors.py
@@ -21,29 +21,29 @@ if __name__ == '__main__':
         rospy.sleep(2.0)
 '''
 
-import rospy
+import rospy, logging, time
 from std_msgs.msg import String
 
 if __name__ == '__main__':
     rospy.init_node("test_behaviors")
-    rospy.loginfo("Sending test commands")
+    rospy.loginfo("launch file executed ") # location: ~/.ros/log/latest/test_cmds-8.log
     pub = rospy.Publisher("/maki_macro", String, queue_size=10)
-    '''
-    default = [ # default behaviors
-        "reset selectiveAttention",
-        "visualScan start"
-    ]
-    '''
+    
     default = [ # default behaviors
         "reset eyelids",
         "reset selectiveAttention",
         "spontaneousBlink start" #"asleep"
     ]
+    current_action = "initialize"
 
+    rospy.loginfo("[CMD-SENT] " + current_action)
+    start_time = time.time()
     for msg in default: 
-        rospy.loginfo(msg)
+        #rospy.loginfo(msg)
         pub.publish(String(msg))
         rospy.sleep(1.0)
+    elapsed_time = time.time() - start_time
+    rospy.loginfo("[CMD-COMPLETE, time elapsed = %ss] %s" % (elapsed_time, current_action))
 
     while True:
         n = raw_input("\n Input message:")
@@ -53,31 +53,45 @@ if __name__ == '__main__':
                 #"reset selectiveAttention",
                 "visualScan start" 
             ]
+            current_action = "think"
         elif (n == "wonder"): # 'wondering' behavior
             msgs = [
                 "lookAwayFromAlissa",
                 "visualScan start"
             ]
+            current_action = "wonder"
         elif (n == "neutral"): # return to neutral position
             msgs = [
                 "lookNeutral",
                 "visualScan stop"
             ]
+            current_action = "neutral"
         elif (n == "shake"): # 'no' behavior
             msgs = [
                 "reset eyelids",
                 "visualScan stop",
                 "shake"
             ]
+            current_action = "shake"
         elif (n == "nod"): # 'yes' behavior
             msgs = [
                 "reset eyelids",
                 "visualScan stop",
                 "nod"
             ]
+            current_action = "nod"
 
+
+        rospy.loginfo("[CMD-SENT] " + current_action)
+        start_time = time.time()
         for msg in msgs:
-            rospy.loginfo(msg)
             pub.publish(String(msg))
             rospy.sleep(1.0)
+        elapsed_time = time.time() - start_time
+        rospy.loginfo("[CMD-COMPLETE, time elapsed = %ss] %s" % (elapsed_time, current_action))
+
+        #for msg in msgs:
+        #    rospy.loginfo(msg)
+        #    pub.publish(String(msg))
+        #    rospy.sleep(1.0)
     
